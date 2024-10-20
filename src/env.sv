6a871e9610e1404dbddba1cddc0c813b1312f92c
Admin <giahuy13032003@gmail.com>
2024-10-20 00:44:14 -0400

    Update env and sb for analysis port

--- src/env.sv
+++ src/env.sv
@@ -4,17 +4,20 @@ class env extends uvm_env;
     super.new(name, parent);
   endfunction
   
-  agent 		a0; 		// Agent handle
+  axi_agent 		axi_a0; 		// Agent handle
+  axil_agent    axil_a0;
   scoreboard	sb0; 		// Scoreboard handle
     
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    a0 = agent::type_id::create("a0", this);
+    axi_a0 = axi_agent::type_id::create("axi_a0", this);
+    axil_a0 = axil_agent::type_id::create("axil_a0", this);
     sb0 = scoreboard::type_id::create("sb0", this);
   endfunction
   
   virtual function void connect_phase(uvm_phase phase);
     super.connect_phase(phase);
-    a0.m0.mon_ap.connect(sb0.m_analysis_imp);
+    axi_a0.m0.axi_mon_ap.connect(sb0.axi_analysis_imp);
+    axil_a0.m0.axi_mon_ap.connect(sb0.axil_analysis_imp);
   endfunction
 endclass
