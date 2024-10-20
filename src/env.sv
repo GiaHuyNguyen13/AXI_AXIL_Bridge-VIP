7a33c9451355601f081d30b4d5b5302f8203a9f7
Admin <giahuy13032003@gmail.com>
2024-10-20 01:49:17 -0400

    Update env to fetch memory to database

--- src/env.sv
+++ src/env.sv
@@ -7,12 +7,15 @@ class env extends uvm_env;
   axi_agent 		axi_a0; 		// Agent handle
   axil_agent    axil_a0;
   scoreboard	sb0; 		// Scoreboard handle
+  centralized_memory_model mem;
     
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     axi_a0 = axi_agent::type_id::create("axi_a0", this);
     axil_a0 = axil_agent::type_id::create("axil_a0", this);
     sb0 = scoreboard::type_id::create("sb0", this);
+    mem = centralized_memory_model::new();
+    uvm_config_db#(centralized_memory_model)::set(this, "*", "central_memory", mem);
   endfunction
   
   virtual function void connect_phase(uvm_phase phase);
